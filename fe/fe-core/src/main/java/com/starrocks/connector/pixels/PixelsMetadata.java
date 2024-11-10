565266d2ad47db2e47f6ff032d4b87ef941fb27c
Zhengjin-Wang <1054129023@qq.com>
2024-11-10 07:20:29 +0000

    fix checkstyle error

--- fe/fe-core/src/main/java/com/starrocks/connector/pixels/PixelsMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/pixels/PixelsMetadata.java
@@ -14,17 +14,8 @@
 
 package com.starrocks.connector.pixels;
 
-import com.starrocks.connector.Connector;
-import com.starrocks.connector.ConnectorContext;
 import com.starrocks.connector.ConnectorMetadata;
 
-public class PixelsConnector implements Connector {
+public class PixelsMetadata implements ConnectorMetadata {
 
-    public PixelsConnector(ConnectorContext context){
-
-    }
-    @Override
-    public ConnectorMetadata getMetadata() {
-        return null;
-    }
 }
